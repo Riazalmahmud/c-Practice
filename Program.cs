9c04532e9b302ac33a202d56913b2fb2f51d70d8
Riazalmahmud <riazalmahmud041@gmail.com>
2025-07-05 16:46:31 +0600

    encapsulations

--- Program.cs
+++ Program.cs
@@ -1,17 +1,70 @@
 ﻿using System;
-namespace TestConstansNamespace
+namespace TestEncapsulationspace
 {
 
-
-    class TesConstans
+    class Person
     {
-
-
-        static void Main(string[] args)
+        private string name;
+        public void setName(string NewName)
         {
-           
-         
+            if (!string.IsNullOrEmpty(NewName))
+            {
+                name = NewName;
+            }
+            else
+            {
+                Console.WriteLine("Name cannot be empty.");
+            }
+        }
 
+        public string GetName()
+        {
+            return name;
+        }
+    }
+
+
+    class EmployeeSalary
+    {
+        private double salary;
+
+        public double Salary
+        {
+            get { return salary; }
+            set
+            {
+                if (value > 3000)
+                {
+                    salary = value;
+                }
+                else
+                {
+                    Console.WriteLine($"salary is lase then  {value}");
+                }
+            }
+        }
+
+        class TestEncapsulation
+        {
+
+
+            static void Main(string[] args)
+            {
+
+                Person person = new Person();
+                person.setName("Riaz al mahmud");
+                Console.WriteLine("get person name " + person.GetName());
+
+                // encapsulation 
+                EmployeeSalary employeeSalary = new EmployeeSalary();
+                employeeSalary.Salary = 50000;
+                Console.WriteLine($"employeeSalary is {employeeSalary.Salary}");
+                EmployeeSalary employeeSalary2 = new EmployeeSalary();
+                employeeSalary2.Salary = 1000;
+                Console.WriteLine($"employeeSalary is : {employeeSalary2.Salary}");
+
+
+            }
         }
     }
 }
---
+++ encapsulation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace FNamespace
+{
+
+
+    class TesConstans
+    {
+
+
+        static void MainEncapsulation(string[] args)
+        {
+           
+         
+
+        }
+    }
+}
