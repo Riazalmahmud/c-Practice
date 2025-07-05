b4e1ab303d3166d3c4d7ce660225c16509aa46dc
Riazalmahmud <riazalmahmud041@gmail.com>
2025-07-05 14:41:43 +0600

    first commit

---
+++ TestRectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace TestRectangle
+{
+
+
+    class TestRectangle
+    {
+        public double length;
+        public double Width;
+
+        public double GetArea()
+        {
+            return Width * length;
+        }
+
+        public void GetDisplayRectangle()
+        {
+            Console.WriteLine("TestRectangle: " + GetArea());
+        }
+    }
+
+
+    class ExecutiveArea
+    {
+
+        public static void MainRect(string[] args)
+        {
+            TestRectangle testRectangle = new TestRectangle();
+            testRectangle.length = 4.5;
+            testRectangle.Width = 3.5;
+            testRectangle.GetDisplayRectangle();
+
+        }
+    }
+    
+}
